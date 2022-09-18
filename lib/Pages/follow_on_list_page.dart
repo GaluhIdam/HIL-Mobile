47522b12ba5c33609144f7f989ecbe118a1973cb
Galuh Idam <galuh.danutirto@opencloud.id>
2022-09-19 01:43:05 +0700

    update add follow on

--- lib/Pages/follow_on_list_page.dart
+++ lib/Pages/follow_on_list_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class FollowOnListPage extends StatelessWidget {
   static const routeName = "/follow_on_list_page";
